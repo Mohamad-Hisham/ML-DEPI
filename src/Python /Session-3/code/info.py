486d5e60115c124cb511a460d0914622d5ab3411
Mohamad-Hisham <mohamedhisham.h.ai@gmail.com>
2026-08-07 02:21:40 +0300

    Add function and info files for user information display

---
+++ src/Python /Session-3/code/info.py
@@ -0,0 +1,17 @@
+# name , age is parameters
+def user_info(name, age):
+    '''
+    This function takes name and age as parameters and prints them in a formatted string.
+
+    Args:
+        param_name (str): The name of the user.
+        param_age (int): The age of the user.
+
+    Returns:
+        return_type: print the user information in a formatted string.
+
+    Example:
+        user_info("John", 30)        
+    '''
+
+    print(f"my name is {name} and my age is {age}")
